01bb44ef58cc6568a7d08ac2874264147faa839b
Bhartii03 <135310137+Bhartii03@users.noreply.github.com>
2025-06-24 13:07:15 +0530

    Update LogoutBtn.jsx

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -2,11 +2,13 @@ import { useDispatch } from "react-redux"
 import authService from "../../appwrite/auth"
 import { logout } from "../../store/authSlice"
 import { useNavigate } from "react-router-dom";
-
+import {useSelector} from "react-redux"
 
 function LogoutBtn(){
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const userData = useSelector((state) => state.auth.userData);
+    const userName = userData?.name || "User";
 
     const logoutHandler = () => {
         authService.logout().then(() => {
@@ -18,7 +20,7 @@ function LogoutBtn(){
     return(
         <button className="inline-block px-6 py-2 duration-200 hover:bg-gray-700 hover:text-white rounded-2xl"
         onClick={logoutHandler}>
-            Logout
+            Logout :{userName}
         </button>
     )
 }
